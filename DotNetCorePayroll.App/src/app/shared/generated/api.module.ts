c4cac4af094d7cfed6e25240b096423245624c01
qinisela elvis molefe <molefeq@hotmail.com>
2018-04-02 10:51:49 +0200

    adding swagger code gen files.

--- DotNetCorePayroll.App/src/app/shared/generated/api.module.ts
+++ DotNetCorePayroll.App/src/app/shared/generated/api.module.ts
@@ -6,25 +6,21 @@ import { Configuration } from './configuration';
 import { AccountService } from './api/account.service';
 
 @NgModule({
-    imports: [
-        CommonModule,
-        HttpClientModule
-    ],
-    declarations: [],
-    exports: [],
-    providers: [
-        AccountService
-    ]
+  imports:      [ CommonModule, HttpClientModule ],
+  declarations: [],
+  exports:      [],
+  providers: [
+    AccountService ]
 })
 export class ApiModule {
     public static forRoot(configurationFactory: () => Configuration): ModuleWithProviders {
         return {
             ngModule: ApiModule,
-            providers: [{ provide: Configuration, useFactory: configurationFactory }]
+            providers: [ { provide: Configuration, useFactory: configurationFactory } ]
         }
     }
 
-    constructor(@Optional() @SkipSelf() parentModule: ApiModule) {
+    constructor( @Optional() @SkipSelf() parentModule: ApiModule) {
         if (parentModule) {
             throw new Error('ApiModule is already loaded. Import your base AppModule only.');
         }
