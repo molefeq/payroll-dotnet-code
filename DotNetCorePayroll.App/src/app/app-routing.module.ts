a1b0a18b97b184619500171f8c761effd5604508
Qinisela Molefe <Qinisela.Molefe@investec.co.za>
2018-04-16 09:36:54 +0200

    Adding security routing and app routing

---
+++ DotNetCorePayroll.App/src/app/app-routing.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes }  from '@angular/router';
+import { PageNotFoundComponent } from './core/page-not-found/page-not-found.component';
+
+const appRoutes: Routes = [
+  { path: '',   redirectTo: '/login', pathMatch: 'full' },
+  { path: '**', component: PageNotFoundComponent }
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(
+      appRoutes
+    )
+  ],
+  exports: [
+    RouterModule
+  ]
+})
+export class AppRoutingModule { }
