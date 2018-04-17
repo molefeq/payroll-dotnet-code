45c9306d5d9f5284dddf617f5dc3d4e3b18232cc
Qinisela Molefe <Qinisela.Molefe@investec.co.za>
2018-04-17 09:16:09 +0200

    Adding the login page.

--- DotNetCorePayroll.App/src/app/components/components.module.ts
+++ DotNetCorePayroll.App/src/app/components/components.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { SecurityModule } from './security/security.module';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   imports: [
-    SecurityModule
+    SecurityModule,
   ],
   declarations: []
 })
--- DotNetCorePayroll.App/src/app/components/security/security.module.ts
+++ DotNetCorePayroll.App/src/app/components/security/security.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login/login.component';
 import { SecurityRoutingModule } from './security-routing.module';
+import { SharedModule } from '../../shared/shared.module';
 
 @NgModule({
   imports: [
     // The module that includes all the basic Angular directives like NgIf, NgForOf, ...
     CommonModule,
-    SecurityRoutingModule
+    SecurityRoutingModule,
+    SharedModule,
   ],
   exports: [
     LoginComponent
